d9b4aeab550799952389497567f30296a25b6f0f
DiacuR <47385825+DiacuR@users.noreply.github.com>
2021-03-21 21:53:36 -0300

    Ejercicio 2. Ruteado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { UsuarioComponent } from './clases/usuario/usuario.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const rutas: Routes = [
+  {
+    path: 'bienvenido',
+    component: BienvenidoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'error',
+    component: ErrorComponent
+  },
+  {
+    path: 'usuario',
+    component: UsuarioComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
